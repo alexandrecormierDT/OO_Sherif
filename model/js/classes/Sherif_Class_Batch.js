bdb0d8f8800fb95ae2039dfa1bfa855a41cafb20
alexandre_ooolala <a.cormier@ooolala.fr>
2021-05-17 10:14:45 +0200

    no message

--- model/js/classes/Sherif_Class_Batch.js
+++ model/js/classes/Sherif_Class_Batch.js
@@ -1,7 +1,7 @@
 window.Sherif.model.Batch = function(){
 	
 	var xstages_path_array  = [];
-	var batch_script;
+	var batch_script_path;
 	var command_string; 
 	
 	function fetch_xstages_paths(){
@@ -18,26 +18,63 @@ window.Sherif.model.Batch = function(){
 		
 	}
 	
-	function fetch_bacth_script(){
+	function fetch_bacth_script_path(){
 		
+		var selected_batch_scripts_ids = window.Sherif.view.bacth_script_row_list.get_selected_bacth_script_id_list(); 
 		
+		var selected_BS = window.Sherif.control.batch_scripts.get_batch_script_object_by_id(selected_batch_scripts_ids[0]);
+		
+		batch_script_path = selected_BS.get_file_path;
 		
 	}	
 	
-	function format_data_form function(){
+	function format_data_form(){
 		
+		fetch_xstages_paths()
 		
+		fetch_bacth_script_path()
+		
+		var xstages_path_string = xstages_path_array.join(",");
+		
+		var data_form = 'batchscript_path='+batch_script_path+'&xstages_paths_str='+xstages_path_string	
+		
+		return data_form;
 		
 	}
 	
-	function send_batch_form = function(){
-		
+	this.send_batch_form = function(){
+		 
+		$.ajax({
+		   url : 'control/php/format_batch_command.php', 
+		   type : 'POST',
+		   data : format_data_form(),
+		   dataType : 'html', 
+		   success : function(code_html, statut){ 
+			   console.log(code_html);
+			   console.log(statut);
+				$('#bacth_status').html(statut);
+				$('#batch_feedback').html(code_html);
+			   
+		   }
+		});			
 		
 	}
 	
 	this.get_command_string = function(){
 		
-		
+		$.ajax({
+		   url : 'control/php/format_batch_command.php', 
+		   type : 'POST',
+		   data : format_data_form(),
+		   dataType : 'html', 
+		   success : function(code_html, statut){ 
+			   console.log(code_html);
+			   console.log(statut);
+				$('#bacth_status').html(statut);
+				$('#batch_feedback').html(code_html);
+			   
+		   }
+		});			
 		
 		
 		
